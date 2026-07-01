47395d654842fe5d194c3640431fc3be852b3491
Blazej Lipinski <dsw60343@student.dsw.edu.pl>
2026-07-01 11:02:30 +0200

    Task 4: Dodano odczyt i walidacje pliku YAML

--- project.py
+++ project.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import os
 import json
+import yaml
 
 def load_data(file_path):
     _, ext = os.path.splitext(file_path)
@@ -19,6 +20,20 @@ def load_data(file_path):
         except Exception as e:
             print(f"Błąd podczas wczytywania pliku: {e}")
             sys.exit(1)
+            
+    elif ext in ['.yml', '.yaml']:
+        try:
+            with open(file_path, 'r', encoding='utf-8') as f:
+                data = yaml.safe_load(f)
+            print("Pomyślnie wczytano plik YAML.")
+            return data
+        except yaml.YAMLError as e:
+            print(f"Błąd: Niepoprawna składnia pliku YAML w {file_path}. Szczegóły: {e}")
+            sys.exit(1)
+        except Exception as e:
+            print(f"Błąd podczas wczytywania pliku: {e}")
+            sys.exit(1)
+            
     else:
         print(f"Błąd: Format {ext} nie jest jeszcze obsługiwany przy wczytywaniu.")
         sys.exit(1)
@@ -35,6 +50,16 @@ def save_data(data, file_path):
         except Exception as e:
             print(f"Błąd podczas zapisu do pliku: {e}")
             sys.exit(1)
+            
+    elif ext in ['.yml', '.yaml']:
+        try:
+            with open(file_path, 'w', encoding='utf-8') as f:
+                yaml.dump(data, f, default_flow_style=False, allow_unicode=True)
+            print(f"Pomyślnie zapisano dane do pliku: {file_path}")
+        except Exception as e:
+            print(f"Błąd podczas zapisu do pliku: {e}")
+            sys.exit(1)
+            
     else:
         print(f"Błąd: Format {ext} nie jest jeszcze obsługiwany przy zapisie.")
         sys.exit(1)
@@ -54,7 +79,10 @@ def main():
         print(f"Błąd: Plik wejściowy '{args.input_file}' nie istnieje!")
         sys.exit(1)
         
+    # Wczytywanie danych z pliku wejściowego
     data = load_data(args.input_file)
+    
+    # Zapisywanie danych do pliku wyjściowego
     save_data(data, args.output_file)
 
 if __name__ == "__main__":
