57c01289352926b784d83201b9498a35936584e3
Blazej Lipinski <dsw60343@student.dsw.edu.pl>
2026-07-01 10:54:26 +0200

    Task 2: Dodano wczytywanie i walidacje pliku json

--- project.py
+++ project.py
@@ -1,12 +1,32 @@
 import argparse
 import sys
 import os
+import json
+
+def load_data(file_path):
+    _, ext = os.path.splitext(file_path)
+    ext = ext.lower()
+
+    if ext == '.json':
+        try:
+            with open(file_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+            print("Pomyślnie wczytano plik JSON.")
+            return data
+        except json.JSONDecodeError as e:
+            print(f"Błąd: Niepoprawna składnia pliku JSON w {file_path}. Szczegóły: {e}")
+            sys.exit(1)
+        except Exception as e:
+            print(f"Błąd podczas wczytywania pliku: {e}")
+            sys.exit(1)
+    else:
+        print(f"Błąd: Format {ext} nie jest jeszcze obsługiwany przy wczytywaniu.")
+        sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(description="Program do konwersji danych między formatami .xml, .json, .yml/.yaml")
-    
-    parser.add_argument("input_file", help="Ścieżka do pliku wejściowego (np. pathFile1.json)")
-    parser.add_argument("output_file", help="Ścieżka do pliku wyjściowego (np. pathFile2.xml)")
+    parser.add_argument("input_file", help="Ścieżka do pliku wejściowego")
+    parser.add_argument("output_file", help="Ścieżka do pliku wyjściowego")
     
     try:
         args = parser.parse_args()
@@ -14,15 +34,12 @@ def main():
         print("Błąd: Nieprawidłowe argumenty. Sposób użycia: project.py pathFile1.x pathFile2.y")
         sys.exit(1)
 
-    print("--- Konwerter Danych ---")
-    print(f"Plik wejściowy: {args.input_file}")
-    print(f"Plik wyjściowy: {args.output_file}")
-    
     if not os.path.isfile(args.input_file):
         print(f"Błąd: Plik wejściowy '{args.input_file}' nie istnieje!")
         sys.exit(1)
         
-    print("Parsowanie argumentów zakończone sukcesem.")
+    data = load_data(args.input_file)
+    print("Dane w pamięci:", data)
 
 if __name__ == "__main__":
     main()
